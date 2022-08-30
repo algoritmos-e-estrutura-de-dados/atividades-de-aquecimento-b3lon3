8e4dd898a48e8197c2009d93516be25caa3f9fac
beloner <belone00@gmail.com>
2022-08-29 21:53:07 -0300

    fracoes e classes

--- classes.py
+++ classes.py
@@ -12,17 +12,44 @@ class Fracao:
 
         return Fracao(num, den)
 
+    def sub(self, fracao):
+        num = (self.numerador * fracao.denominador) - (fracao.numerador * self.denominador)
+        den = self.denominador * fracao.denominador
+
+        return Fracao(num, den)
+
+    def mult(self, fracao):
+        num = self.numerador * fracao.numerador
+        den = self.denominador * fracao.denominador
+
+        return Fracao(num,den)
+
+    def simplify(self):
+        #tentei
+        if self.numerador % self.denominador == 0 and self.denominador % self.numerador == 0:
+            num = self.numerador / self.denominador
+            den = self.denominador / self.numerador
+
+            return (num,"/",den)
 
     def solve(self):
-        return self.numerador/self.denominador
+        return self.numerador / self.denominador
 
     def __str__(self):
         return f"{self.numerador}/{self.denominador}"
 
+
 fracao1 = Fracao(1, 2)
 fracao2 = Fracao(1, 3)
 fracao3 = fracao1.add(fracao2)
+fracao4 = fracao2.sub(fracao1)
+fracao5 = fracao1.mult(fracao2)
+
+fracao6 = Fracao(2,4)
 
 print(fracao3)
-print(fracao3.solve())
+print(fracao4)
+print(fracao5)
 
+fracao7 = fracao6.simplify()
+print(fracao7)
