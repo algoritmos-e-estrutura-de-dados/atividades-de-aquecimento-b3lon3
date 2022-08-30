92ab3a132bffae3b90dbd3d6f1ab77171876f93e
beloner <belone00@gmail.com>
2022-08-29 21:25:07 -0300

    fracoes e classes

--- classes.py
+++ classes.py
@@ -12,6 +12,10 @@ class Fracao:
 
         return Fracao(num, den)
 
+
+    def solve(self):
+        return self.numerador/self.denominador
+
     def __str__(self):
         return f"{self.numerador}/{self.denominador}"
 
@@ -20,4 +24,5 @@ fracao2 = Fracao(1, 3)
 fracao3 = fracao1.add(fracao2)
 
 print(fracao3)
+print(fracao3.solve())
 
